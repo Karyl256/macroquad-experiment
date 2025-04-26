ef6583ce543b9e5ff3201bf7326c92c71bb90c6c
Karyl <tymczasowymailkarolatymczasowy@gmail.com>
2025-04-26 21:17:34 +0200

    Put physics speed and frame controll in game engine

--- src/game_engine.rs
+++ src/game_engine.rs
@@ -1,14 +1,26 @@
+use macroquad::prelude::*;
 
-
+pub const GRAVITY: Vec2 = Vec2::new(0.0, 1000.0);
+//How fast games physics run
+pub const PHYSICS_SPEED: f32 = 1.0;
+//What is expected amount of fps
+pub const PHYSICS_TARGET_FPS: f32 = 120.0;
+pub const PHYSICS_TARGET_FRAMETIME: f32 = 1.0 / PHYSICS_TARGET_FPS * PHYSICS_SPEED;
+//Maximum of how many frames can you try to gain back to be on time in a frame
+pub const MAX_PHYSICS_UPDATES_PER_FRAME: u32 = 10;
 
 pub mod game_engine {
     use macroquad::prelude::*;
 
     use crate::physics_obj::physics_obj::PhysicsObj;
+    use super::*;
 
     #[derive(Default)]
     pub struct Game {
         ball: PhysicsObj,
+
+        physics_objects: Vec<PhysicsObj>,
+        physics_buffered_time: f32,
     }
 
     impl Game {
@@ -24,7 +36,16 @@ pub mod game_engine {
             created_game
         }
         pub fn physics(&mut self) {
-            self.ball.run_physics();
+            //Buffered time
+            self.physics_buffered_time += get_frame_time() * PHYSICS_SPEED;
+
+            let dt: f32 = PHYSICS_TARGET_FRAMETIME;
+
+            let iteration = 0;
+            while self.physics_buffered_time > PHYSICS_TARGET_FRAMETIME && iteration < MAX_PHYSICS_UPDATES_PER_FRAME {
+                self.ball.run_physics(dt, &self.physics_objects);
+                self.physics_buffered_time -= PHYSICS_TARGET_FRAMETIME;
+            }
         }
         pub fn draw(&mut self) {
             draw_circle(self.ball.position.x, self.ball.position.y, self.ball.radius, RED);
--- src/physics_obj.rs
+++ src/physics_obj.rs
@@ -1,13 +1,8 @@
-use macroquad::prelude::*;
-
-pub const GRAVITY: Vec2 = Vec2::new(0.0, 1000.0);
-pub const SUBSTEP: u32 = 10;
-pub const PHYSICS_SPEED: u32 = 1;
-
 pub mod physics_obj {
+
     use macroquad::prelude::*;
 
-    use super::{GRAVITY, PHYSICS_SPEED, SUBSTEP};
+    use crate::game_engine::GRAVITY;
 
     #[derive(Default)]
     pub struct PhysicsObj {
@@ -37,21 +32,18 @@ pub mod physics_obj {
         }
 
         #[allow(dead_code)]
-        pub fn run_physics(&mut self) {
-            let dt: f32 = 0.01 / super::SUBSTEP as f32;
-
+        pub fn run_physics(&mut self, dt: f32, _list: &Vec<PhysicsObj>) {
             #[allow(unused_mut)]
             let mut acceleration = GRAVITY;
-
-            for _n in 0..(PHYSICS_SPEED * SUBSTEP) {
-                if self.position.y + self.radius > screen_height() {
-                    self.velocity.y = -self.velocity.y;
-                    self.position.y = screen_height() - self.radius;
-                }
-
-                self.velocity += acceleration * dt;
-                self.position += self.velocity * dt;
+            
+            if self.position.y + self.radius > screen_height() {
+                self.velocity.y = -self.velocity.y;
+                self.position.y = screen_height() - self.radius;
             }
+
+            self.velocity += acceleration * dt;
+            self.position += self.velocity * dt;
+            
         }
     }
 }
